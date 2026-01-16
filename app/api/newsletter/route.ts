6181d197fff4e2e60d3e186b105c20abdbd7ae0e
Lilith Harutyunyan <lilith.harutyunyan@cellese.com>
2026-01-15 17:37:55 -0800

    Studio clean

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const POST = async (request: Request) => {
   const { email } = await request.json();
 
-  Create contact
+ 
   try {
     resend.contacts.create({
       email,
